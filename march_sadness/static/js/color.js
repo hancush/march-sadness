2e48c212046ba6ea0d0eda4cb767ef1599ac926b
Hannah Cushman <hannah.cushman@gmail.com>
2018-02-18 08:16:47 -0600

    js enhance

--- march_sadness/static/js/color.js
+++ march_sadness/static/js/color.js
@@ -4,21 +4,20 @@ $('#color-pick').colorpicker({
   'color': initial_color,
 });
 
-var set_background_color = function(color) {
-  $('body').css({'background-color': color});
+var make_text_readable = function(color) {
+  var dark_secondary_light = ["#212531", "#6c757d", "#f8f9fa"];
+  var best = tinycolor.mostReadable(color, dark_secondary_light).toHexString();
+  $('body').css({'color': best});
 };
 
-var make_text_readable = function(color) {
-  var dark_light_secondary = ["#212531", "#6c757d", "#f8f9fa"];
-  var best = tinycolor.mostReadable(color, dark_light_secondary).toHexString();
-  $('body').css({'color': best});
-}
+var set_background_color = function(color) {
+  $('body').css({'background-color': color});
+  make_text_readable(color);
+};
 
 set_background_color(initial_color);
-make_text_readable(initial_color);
 
 $('body').on('colorpickerChange', function(e) {
   var selected_color = e.color.toRgbString();
   set_background_color(selected_color);
-  make_text_readable(selected_color);
 });
