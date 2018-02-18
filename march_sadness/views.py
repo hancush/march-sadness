3078b08cbd5fc5e9ee1f603c2e8c4789bffbb109
Hannah Cushman <hannah.cushman@gmail.com>
2018-02-18 05:38:06 -0600

    Move ncds to its own repo

--- march_sadness/views.py
+++ march_sadness/views.py
@@ -6,3 +6,16 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def index():
     return 'HI'
+
+
+@views.route('/data', methods=['GET'])
+def data():
+    base_url = 'http://games.espn.com/tournament-challenge-bracket/{year}/en/nationalBracket'
+
+    page = requests.get(base_url.format(year=str(year)))
+    tree = etree.HTML(page.text)
+
+    script, = tree.xpath('//div[@id="global-viewport"]/following::script[1]')
+    data = script.text.partition('scoreboard_teams = [')[2].partition(']')[0]
+
+    return json.loads('[{}]'.format(data))
